8641233908e300bbb38baf6f6b3430fd6ca31f3d
Jesse Wei <jesseweijapan@gmail.com>
2025-10-15 17:07:25 +1000

    remove added project

--- BoardGame/DiscType.cs
+++
@@ -1,7 +0,0 @@
-public enum DiscType
-{
-  Ordinary,
-  Boring,
-  Magnetic,
-  Exploding
-}
--- BoardGame/Mode.cs
+++
@@ -1,8 +0,0 @@
-/*
-  Enum is a structured data type that makes it easier to read, extend, and maintain code.
-*/
-public enum Mode
-{
-  HumanToHuman,
-  HumanToAI,
-}
